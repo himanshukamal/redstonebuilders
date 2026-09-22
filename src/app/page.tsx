c3b81cb316e1c6d185ce37cde7efe6733d8714bb
Himanshu-Home <himanshu.55421@gmail.com>
2026-09-22 12:16:31 +0530

    removed bulid error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// import BrandImageCarousel from "./components/BrandImageCarousel";
 import BrandImageCarousel from "./components/BrandImageCarousel";
 import CostEstimation from "./components/CostEstimation";
 import Header from "./components/Header";
